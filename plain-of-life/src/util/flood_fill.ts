df089f2cc428304a06999cad04a59c54f75e1c6e
abrossler <abrossler@gmx.de>
2021-12-09 20:53:57 +0100

    Final flood fill

--- plain-of-life/src/util/flood_fill.ts
+++ plain-of-life/src/util/flood_fill.ts
@@ -1,60 +1,83 @@
-// From https://github.com/pavelkukov/q-floodfill/blob/master/src/FloodFill.ts
+// Inspired by https://github.com/pavelkukov/q-floodfill/blob/master/src/FloodFill.ts
 
-// /**
-//  * [startX, endX, y, parentY]
-//  */
-type QueuedLine = [number, number, number, number]
-
+/**
+ * A flood fill implementation filling an area of a plain considering 4 directions (left, right, top and bottom).
+ *
+ * For the plain a torus topography is assumed - leaving the plain at the top, you re-enter
+ * it at the bottom, leaving it to the right, you re-enter it from the left. And vice versa...
+ */
 export class FloodFill<T> {
-  public plainToFill: T[][]
+  private plainToFill: T[][]
   private plainWidth: number
   private plainHeight: number
-  public replacedCount = 0
-  private queue: Array<QueuedLine> = []
-  private toReplace!: T
-  private replaceWith!: T
 
-  constructor(plainToFill: T[][]) {
+  // Number of replacements on the plain with the latest fill run
+  private replacedCount = 0
+
+  // Queue with information on lines during a fill run: minX, maxX, y, parentY
+  private queue: Array<[number, number, number, number | undefined]> = []
+
+  // Maximum number of tapings a shape can have in a torus topography (higher numbers break the flood fill algorithm)
+  private static maxTaping = 256
+
+  /**
+   * @param plainToFill A 2D array of an arbitrary type T to be flood filled. Each line of the array is expected to have the same length
+   * @param isEqual An optional custom comparison function - with {@link defaultIsEqual} as default
+   * @param replace An optional custom replacement function - with {@link defaultReplace} as default
+   */
+  constructor(
+    plainToFill: T[][],
+    private isEqual: (t1: T, t2: T) => boolean = defaultIsEqual,
+    private replace: (plainToFill: T[][], replaceWith: T, x: number, y: number) => void = defaultReplace
+  ) {
     this.plainToFill = plainToFill
     this.plainWidth = plainToFill[0].length
     this.plainHeight = plainToFill.length
   }
 
-  public fill(replaceWith: T, x: number, y: number): void {
-    this.replaceWith = replaceWith
-    this.toReplace = this.plainToFill[y][x]
+  /**
+   * Flood fill a plain starting at (x, y): Every neighbor of (x,y) on the plain that is equal to the current (x,y)
+   * is replaced by replaceWith
+   * @returns The number of points that was filled
+   */
+  public fill(replaceWith: T, x: number, y: number): number {
+    this.replacedCount = 0
+    const toReplace = this.plainToFill[y][x]
 
-    if (this.replaceWith === this.toReplace) {
-      return
+    if (replaceWith === toReplace) {
+      return this.replacedCount
     }
 
-    this.queue.push([x, x, y, -1])
-    this.fillQueue()
+    // Needed for support of torus topography
+    x += FloodFill.maxTaping * this.plainWidth // Avoid x to become < 0 when leaving the plain to the left
+    y += FloodFill.maxTaping * this.plainHeight // Avoid y to become < 0 when leaving the plain to the top
+
+    this.queue.push([x, x, y, undefined])
+    this.fillQueue(toReplace, replaceWith)
+    return this.replacedCount
   }
 
-  private fillQueue(): void {
+  /**
+   * Helper to fill the queue with lines to be processed
+   */
+  private fillQueue(toReplace: T, replaceWith: T): void {
     let line = this.queue.pop()
     while (line) {
       const [minX, maxX, y, parentY] = line
       let currX = minX
-      while (/*currX !== -1 &&*/ currX <= maxX) {
-        // currX !== -1 most likely not needed
-        const [lineStart, lineEnd] = this.fillLineAt(currX, y)
+      while (currX <= maxX) {
+        const [lineStart, lineEnd] = this.fillLineAt(currX, y, toReplace, replaceWith)
         if (lineStart !== undefined && lineEnd !== undefined) {
-          if (lineStart >= minX && lineEnd <= maxX && parentY !== -1) {
-            if (parentY < y && y + 1 < this.plainHeight) {
+          if (lineStart >= minX && lineEnd <= maxX && parentY !== undefined) {
+            if (parentY < y) {
               this.queue.push([lineStart, lineEnd, y + 1, y])
             }
-            if (parentY > y && y > 0) {
+            if (parentY > y) {
               this.queue.push([lineStart, lineEnd, y - 1, y])
             }
           } else {
-            if (y > 0) {
-              this.queue.push([lineStart, lineEnd, y - 1, y])
-            }
-            if (y + 1 < this.plainHeight) {
-              this.queue.push([lineStart, lineEnd, y + 1, y])
-            }
+            this.queue.push([lineStart, lineEnd, y - 1, y])
+            this.queue.push([lineStart, lineEnd, y + 1, y])
           }
         }
         if (lineEnd === undefined && currX <= maxX) {
@@ -67,32 +90,147 @@ export class FloodFill<T> {
     }
   }
 
-  private fillLineAt(x: number, y: number): [number | undefined, number | undefined] {
-    if (this.plainToFill[y][x] !== this.toReplace) {
+  /**
+   * Helper to actually fill a line from the queue
+   * @returns min and max x of the line
+   */
+  private fillLineAt(x: number, y: number, toReplace: T, replaceWith: T): [number | undefined, number | undefined] {
+    const adjustedY = y % this.plainHeight
+
+    if (!this.isEqual(this.plainToFill[adjustedY][x % this.plainWidth], toReplace)) {
       return [undefined, undefined]
     }
-    this.replace(this.replaceWith, x, y)
+    this.replace(this.plainToFill, replaceWith, x % this.plainWidth, adjustedY)
+    this.replacedCount++
     let minX = x
     let maxX = x
 
     let currX = minX - 1
-    while (currX >= 0 && this.plainToFill[y][currX] === this.toReplace) {
-      this.replace(this.replaceWith, currX, y)
-      minX = currX
-      currX -= 1
+    let adjustedX = currX % this.plainWidth
+    while (this.isEqual(this.plainToFill[adjustedY][adjustedX], toReplace)) {
+      this.replace(this.plainToFill, replaceWith, adjustedX, adjustedY)
+      this.replacedCount++
+      minX = currX--
+      adjustedX = currX % this.plainWidth
     }
 
     currX = maxX + 1
-    while (currX < this.plainWidth && this.plainToFill[y][currX] === this.toReplace) {
-      this.replace(this.replaceWith, currX, y)
-      maxX = currX
-      currX += 1
+    adjustedX = currX % this.plainWidth
+    while (this.isEqual(this.plainToFill[adjustedY][adjustedX], toReplace)) {
+      this.replace(this.plainToFill, replaceWith, adjustedX, adjustedY)
+      this.replacedCount++
+      maxX = currX++
+      adjustedX = currX % this.plainWidth
     }
     return [minX, maxX]
   }
-
-  private replace(color: T, x: number, y: number): void {
-    this.plainToFill[y][x] = color
-    this.replacedCount++
-  }
 }
+
+/**
+ * Default === based comparison implementation
+ */
+function defaultIsEqual<T>(t1: T, t2: T): boolean {
+  return t1 === t2
+}
+
+/**
+ * Default replace implementation just replacing a point on the plain by replaceWith.
+ */
+function defaultReplace<T>(plainToFill: T[][], replaceWith: T, x: number, y: number): void {
+  plainToFill[y][x] = replaceWith
+}
+
+// For reference - flood fill with non-torus topography
+// type QueuedLine = [number, number, number, number]
+
+// export class FloodFill<T> {
+//   public plainToFill: T[][]
+//   private plainWidth: number
+//   private plainHeight: number
+//   public replacedCount = 0
+//   private queue: Array<QueuedLine> = []
+//   private toReplace!: T
+//   private replaceWith!: T
+
+//   constructor(plainToFill: T[][]) {
+//     this.plainToFill = plainToFill
+//     this.plainWidth = plainToFill[0].length
+//     this.plainHeight = plainToFill.length
+//   }
+
+//   public fill(replaceWith: T, x: number, y: number): void {
+//     this.replaceWith = replaceWith
+//     this.toReplace = this.plainToFill[y][x]
+
+//     if (this.replaceWith === this.toReplace) {
+//       return
+//     }
+
+//     this.queue.push([x, x, y, -1])
+//     this.fillQueue()
+//   }
+
+//   private fillQueue(): void {
+//     let line = this.queue.pop()
+//     while (line) {
+//       const [minX, maxX, y, parentY] = line
+//       let currX = minX
+//       while (/*currX !== -1 &&*/ currX <= maxX) {
+//         // currX !== -1 most likely not needed
+//         const [lineStart, lineEnd] = this.fillLineAt(currX, y)
+//         if (lineStart !== undefined && lineEnd !== undefined) {
+//           if (lineStart >= minX && lineEnd <= maxX && parentY !== -1) {
+//             if (parentY < y && y + 1 < this.plainHeight) {
+//               this.queue.push([lineStart, lineEnd, y + 1, y])
+//             }
+//             if (parentY > y && y > 0) {
+//               this.queue.push([lineStart, lineEnd, y - 1, y])
+//             }
+//           } else {
+//             if (y > 0) {
+//               this.queue.push([lineStart, lineEnd, y - 1, y])
+//             }
+//             if (y + 1 < this.plainHeight) {
+//               this.queue.push([lineStart, lineEnd, y + 1, y])
+//             }
+//           }
+//         }
+//         if (lineEnd === undefined && currX <= maxX) {
+//           currX += 1
+//         } else if (lineEnd !== undefined) {
+//           currX = lineEnd + 1
+//         }
+//       }
+//       line = this.queue.pop()
+//     }
+//   }
+
+//   private fillLineAt(x: number, y: number): [number | undefined, number | undefined] {
+//     if (this.plainToFill[y][x] !== this.toReplace) {
+//       return [undefined, undefined]
+//     }
+//     this.replace(this.replaceWith, x, y)
+//     let minX = x
+//     let maxX = x
+
+//     let currX = minX - 1
+//     while (currX >= 0 && this.plainToFill[y][currX] === this.toReplace) {
+//       this.replace(this.replaceWith, currX, y)
+//       minX = currX
+//       currX -= 1
+//     }
+
+//     currX = maxX + 1
+//     while (currX < this.plainWidth && this.plainToFill[y][currX] === this.toReplace) {
+//       this.replace(this.replaceWith, currX, y)
+//       maxX = currX
+//       currX += 1
+//     }
+//     return [minX, maxX]
+//   }
+
+//   private replace(color: T, x: number, y: number): void {
+//     this.plainToFill[y][x] = color
+//     this.replacedCount++
+//   }
+// }
--- plain-of-life/tests/util/flood_fill.test.ts
+++ plain-of-life/tests/util/flood_fill.test.ts
@@ -23,6 +23,54 @@ describe('FloodFill', () => {
     }
   })
 
+  it('supports custom isEqual comparator correctly', () => {
+    const plainToFill = [
+      [4, 4, 4, 4, 4, 4, 4],
+      [4, 1, 2, 1, 2, 3, 4],
+      [4, 1, 2, 1, 2, 3, 4],
+      [4, 1, 2, 1, 2, 3, 4],
+      [4, 4, 4, 4, 4, 4, 4]
+    ]
+    const fF = new FloodFill(plainToFill, (n1, n2) => n1 <= n2)
+    fF.fill(9, 2, 2)
+    const filledPlain = [
+      [4, 4, 4, 4, 4, 4, 4],
+      [4, 9, 9, 9, 9, 3, 4],
+      [4, 9, 9, 9, 9, 3, 4],
+      [4, 9, 9, 9, 9, 3, 4],
+      [4, 4, 4, 4, 4, 4, 4]
+    ]
+    for (let i = 0; i < plainToFill.length; i++) {
+      expect(plainToFill[i]).toEqual(filledPlain[i])
+    }
+  })
+
+  it('supports custom replace and other elements than numbers correctly', () => {
+    const plainToFill = [
+      ['AA', 'AA', 'AA', 'AA', 'AA', 'AA'],
+      ['AA', 'BA', 'BA', 'BA', 'BA', 'AA'],
+      ['AA', 'BA', 'BB', 'BB', 'BA', 'AA'],
+      ['AA', 'BA', 'BA', 'BA', 'BA', 'AA'],
+      ['AA', 'AA', 'AA', 'AA', 'AA', 'AA']
+    ]
+    const fF = new FloodFill(
+      plainToFill,
+      (n1, n2) => n1.charAt(0) === n2.charAt(0),
+      (plainToFill, replaceWith, x, y) => (plainToFill[y][x] = replaceWith + plainToFill[y][x].substring(1))
+    )
+    fF.fill('C', 1, 1)
+    const filledPlain = [
+      ['AA', 'AA', 'AA', 'AA', 'AA', 'AA'],
+      ['AA', 'CA', 'CA', 'CA', 'CA', 'AA'],
+      ['AA', 'CA', 'CB', 'CB', 'CA', 'AA'],
+      ['AA', 'CA', 'CA', 'CA', 'CA', 'AA'],
+      ['AA', 'AA', 'AA', 'AA', 'AA', 'AA']
+    ]
+    for (let i = 0; i < plainToFill.length; i++) {
+      expect(plainToFill[i]).toEqual(filledPlain[i])
+    }
+  })
+
   it('fills outline correctly', () => {
     const plainToFill = [
       [0, 0, 0, 0, 0, 0, 0],
@@ -123,31 +171,79 @@ describe('FloodFill', () => {
     }
   })
 
-  //   it('handles torus topography correctly', () => {
-  //     const plainToFill = [
-  //         [0, 0, 1, 0, 1, 0, 1, 0, 1],
-  //         [0, 1, 1, 0, 1, 0, 1, 0, 1],
-  //         [0, 0, 0, 0, 1, 1, 1, 0, 1],
-  //         [1, 0, 0, 0, 0, 0, 0, 1, 1],
-  //         [1, 0, 0, 0, 0, 0, 0, 0, 0],
-  //         [1, 0, 0, 0, 0, 0, 0, 1, 1],
-  //         [0, 0, 0, 0, 0, 0, 1, 1, 0],
-  //         [0, 0, 1, 1, 1, 0, 1, 1, 0]
-  //     ]
-  //     let fF = new FloodFill(plainToFill)
-  //     fF.fill(9, 1, 1)
-  //     const filledPlain = [
-  //         [0, 0, 9, 0, 9, 0, 9, 0, 9],
-  //         [0, 9, 9, 0, 9, 0, 9, 0, 9],
-  //         [0, 0, 0, 0, 9, 9, 9, 0, 9],
-  //         [9, 0, 0, 0, 0, 0, 0, 9, 9],
-  //         [9, 0, 0, 0, 0, 0, 0, 0, 0],
-  //         [9, 0, 0, 0, 0, 0, 0, 9, 9],
-  //         [0, 0, 0, 0, 0, 0, 9, 9, 0],
-  //         [0, 0, 9, 9, 9, 0, 9, 9, 0]
-  //     ]
-  //     for (let i = 0; i < plainToFill.length; i++) {
-  //       expect(plainToFill[i]).toEqual(filledPlain[i])
-  //     }
-  //   })
+  it('handles torus topography on x-axis correctly', () => {
+    const plainToFill = [
+      [1, 0, 0, 1, 1],
+      [1, 0, 0, 0, 0],
+      [1, 0, 1, 1, 1],
+      [1, 0, 0, 0, 0],
+      [1, 0, 1, 1, 1],
+      [0, 0, 0, 0, 0]
+    ]
+    const fF = new FloodFill(plainToFill)
+    fF.fill(9, 2, 2)
+    const filledPlain = [
+      [9, 0, 0, 9, 9],
+      [9, 0, 0, 0, 0],
+      [9, 0, 9, 9, 9],
+      [9, 0, 0, 0, 0],
+      [9, 0, 9, 9, 9],
+      [0, 0, 0, 0, 0]
+    ]
+    for (let i = 0; i < plainToFill.length; i++) {
+      expect(plainToFill[i]).toEqual(filledPlain[i])
+    }
+  })
+
+  it('handles torus topography on y-axis correctly', () => {
+    const plainToFill = [
+      [1, 0, 1, 0, 1, 1, 1, 1],
+      [1, 0, 1, 0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 0, 0, 1, 1],
+      [0, 0, 0, 1, 1, 0, 1, 1],
+      [0, 0, 0, 0, 0, 0, 1, 1],
+      [1, 1, 1, 1, 1, 0, 1, 1]
+    ]
+    const fF = new FloodFill(plainToFill)
+    fF.fill(9, 2, 2)
+    const filledPlain = [
+      [9, 0, 9, 0, 9, 9, 9, 9],
+      [9, 0, 9, 0, 0, 0, 0, 0],
+      [0, 0, 9, 0, 0, 0, 9, 9],
+      [0, 0, 0, 1, 1, 0, 9, 9],
+      [0, 0, 0, 0, 0, 0, 9, 9],
+      [9, 9, 9, 9, 9, 0, 9, 9]
+    ]
+    for (let i = 0; i < plainToFill.length; i++) {
+      expect(plainToFill[i]).toEqual(filledPlain[i])
+    }
+  })
+
+  it('handles complex torus topography correctly', () => {
+    const plainToFill = [
+      [0, 0, 1, 0, 1, 0, 1, 0, 1],
+      [0, 1, 1, 0, 1, 0, 1, 0, 1],
+      [0, 0, 0, 0, 1, 1, 1, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 0, 0, 0, 0, 1, 0, 1, 1],
+      [0, 1, 0, 0, 0, 0, 1, 1, 0],
+      [0, 0, 1, 1, 1, 0, 1, 1, 0]
+    ]
+    const fF = new FloodFill(plainToFill)
+    fF.fill(9, 1, 1)
+    const filledPlain = [
+      [0, 0, 9, 0, 9, 0, 9, 0, 9],
+      [0, 9, 9, 0, 9, 0, 9, 0, 9],
+      [0, 0, 0, 0, 9, 9, 9, 0, 9],
+      [9, 0, 0, 1, 0, 0, 0, 9, 9],
+      [9, 0, 0, 0, 0, 0, 0, 0, 0],
+      [9, 0, 0, 0, 0, 1, 0, 9, 9],
+      [0, 1, 0, 0, 0, 0, 9, 9, 0],
+      [0, 0, 9, 9, 9, 0, 9, 9, 0]
+    ]
+    for (let i = 0; i < plainToFill.length; i++) {
+      expect(plainToFill[i]).toEqual(filledPlain[i])
+    }
+  })
 })
